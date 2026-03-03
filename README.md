6960ff7c8fa39af11dab09313dcf87e69d93b01f
Federico Ferretti <ricofede6@gmail.com>
2026-03-03 16:03:35 +0100

    Add initial implementation for FIFASquadFileDownloader
    - Created main script to download latest EAFC 26 squad files for various platforms.
    - Added LICENSE file with MIT License.
    - Included README.md for project description and usage instructions.
    - Implemented functionality to handle squad and FUT files for PS4 and PS5.
    - Introduced binreader module for reading binary data.
    - Added legacy_unpack module for unpacking specific file formats.
    - Created .gitignore to exclude unnecessary files and directories.
    - Added rosterupdate.xml with platform-specific squad information.

---
+++ README.md
@@ -0,0 +1,15 @@
+# FIFASquadFileDownloader
+
+Python script which downloads latest EAFC 26 squadfile and futsquadfile from game servers for platforms:
+- PC64
+- NX (nintendo switch?)
+- sta (stadia)
+- XONE
+- XBSX
+- PS4
+- PS5
+
+
+You don't need to run the game and it also doesn't require any credentials.
+
+Output is stored in "result" folder in the same directory where you have the script.
---
+++ main.py
@@ -0,0 +1,358 @@
+import os
+import urllib.request
+import xml.etree.ElementTree as ET
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# CONTENT_URL = "https://fifa21.content.easports.com/fifa/fltOnlineAssets/21D4F1AC-91A3-458D-A64E-895AA6D871D1/2021/"
+# CONTENT_URL = "https://fifa22.content.easports.com/fifa/fltOnlineAssets/22747632-e3df-4904-b3f6-bb0035736505/2022/"
+# CONTENT_URL = "https://fifa23.content.easports.com/fifa/fltOnlineAssets/23DF3AC5-9539-438B-8414-146FAFDE3FF2/2023/"
+# CONTENT_URL = "https://eafc24.content.easports.com/fc/fltOnlineAssets/24B23FDE-7835-41C2-87A2-F453DFDB2E82/2024/"
+# CONTENT_URL = "https://eafc25.content.easports.com/fc/fltOnlineAssets/25E4CDAE-799B-45BE-B257-667FDCDE8044/2025/"
+CONTENT_URL = "https://eafc26.content.easports.com/fc/fltOnlineAssets/26E4D4D6-8DBB-4A9A-BD99-9C47D3AA341D/2026/"
+ROSTERUPDATE_XML = "rosterupdate.xml"
+
+# signs
+T3DB = b"\x44\x42\x00\x08"
+FBCHUNKS = b"\x46\x42\x43\x48\x55\x4E\x4B\x53\x01\x00"
+BNRY = b"\x42\x4E\x52\x59\x00\x00\x00\x02\x4C\x54\x4C\x45\x01\x01\x03\x00" \
+       b"\x00\x00\x63\x64\x73\x01\x00\x00\x00\x00\x01\x03\x00\x00\x00\x63\x64\x73"
+
+RESULT_DIR = "result"
+DATA_PATH = "DATA"
+
+ALLOWED_PLATFORMS = {"ps4", "ps5"}
+
+
+def download(fpath, url):
+    print("Download: {}".format(url))
+    with open(fpath, "wb") as f:
+        try:
+            response = urllib.request.urlopen(url)
+            f.write(response.read())
+        except Exception as e:
+            print(e)
+
+def download_rosterupdate():
+    roster_update_url = "{}fc/fclive/genxtitle/rosterupdate.xml".format(CONTENT_URL)
+    download(ROSTERUPDATE_XML, roster_update_url)
+
+def process_rosterupdate():
+    result = dict()
+    to_collect = [
+        "dbMajor", "dbFUTVer", "dbMajorLoc", "dbFUTLoc"
+    ]
+    
+    download_rosterupdate()
+    tree = ET.parse(ROSTERUPDATE_XML)
+    root = tree.getroot()
+    
+    try:
+        squadinfoset = root[0]
+        result["platforms"] = list()
+        for child in squadinfoset:
+            platform = dict()
+            platform_name = child.attrib["platform"]
+            platform["name"] = platform_name
+            platform["tags"] = dict()
+            for node in list(child.iter()):
+                if node.tag in to_collect:
+                    platform["tags"][node.tag] = node.text
+            
+            result["platforms"].append(platform)
+    except Exception as e:
+        return result
+    
+    return result
+
+
+def save_squads(buf, path, filename):
+    fullpath = os.path.join(path, filename)
+    is_fut = "Fut" in filename
+    db_size = len(buf)
+    
+    # Save types
+    save_type_squads = b"SaveType_Squads\x00"
+    save_type_fut = b"SaveType_FUTSqu\x00"
+
+    author_sign = b"Aranaktu"
+
+    # FC26 chunk sizes
+    prefix_header_size = 1126
+    main_header_size = 48
+    bnry_size = 45985
+
+    # Calculate file size
+    bnry_size = 0 if is_fut else bnry_size
+    file_size = main_header_size + 4 + db_size + bnry_size
+    
+    # Build prefix header
+    prefix_header = bytearray(prefix_header_size)
+    pos = 0
+    
+    # FBCHUNKS sign
+    prefix_header[pos:pos+len(FBCHUNKS)] = FBCHUNKS
+    pos += len(FBCHUNKS)
+    
+    # Main header offset
+    main_header_offset = prefix_header_size - pos - 8
+    prefix_header[pos:pos+4] = main_header_offset.to_bytes(4, "little")
+    pos += 4
+    
+    # File size excluding prefix header
+    prefix_header[pos:pos+4] = file_size.to_bytes(4, "little")
+    pos += 4
+    
+    # In-game name 
+    ingame_name = "EA_{}".format(filename).encode()[:40] # max to 40 bytes
+    prefix_header[pos:pos+len(ingame_name)] = ingame_name
+    pos += len(ingame_name)
+
+    # Author sign
+    sign_size = 4 if is_fut else 7
+    pos += sign_size
+    prefix_header[pos:pos+len(author_sign)] = author_sign 
+    
+    # Build main header
+    main_header = bytearray(main_header_size)
+
+    # Save type
+    save_type = save_type_fut if is_fut else save_type_squads
+    main_header[:len(save_type)] = save_type
+    
+    # CRC32
+    crc_pos = len(save_type)
+    main_header[crc_pos:crc_pos+4] = (0).to_bytes(4, "little") 
+
+    # Calculate data section size
+    data_size = 0 if is_fut else db_size + bnry_size
+    
+    with open(fullpath, "wb") as f:
+        # Write prefix header
+        f.write(bytes(prefix_header))
+        
+        # Write main header
+        f.write(bytes(main_header))
+        
+        # Write data section size
+        f.write(data_size.to_bytes(4, "little"))
+        
+        # Write DB
+        f.write(buf)
+        
+        # Write BNRY chunk only for Squads
+        if not is_fut:
+            f.write(BNRY)
+            remaining_bnry = bnry_size - len(BNRY)
+            f.write(b"\x00" * remaining_bnry)
+    
+    return filename
+
+def unpack(fpath):
+    print("Unpacking: {}".format(fpath))
+    
+    # Control masks
+    SHORT_COPY = 0x80
+    MEDIUM_COPY = 0x40
+    LONG_COPY = 0x20
+    
+    # Read input file
+    with open(fpath, "rb") as f:
+        data = f.read()
+    
+    # Initialize output buffer
+    size = int.from_bytes(data[2:5], "big")
+    outbuf = bytearray(size)
+    outbuf[:len(T3DB)] = T3DB
+    
+    ipos = 10  # start pos
+    opos = len(T3DB)
+    in_len, out_len = len(data), len(outbuf)
+    last_control = 0
+    
+    while ipos < in_len and opos < out_len:
+        control = data[ipos]
+        last_control = control
+        ipos += 1
+        
+        if not (control & SHORT_COPY):
+            b1 = data[ipos]
+            ipos += 1
+            lit = control & 3
+            if lit:
+                outbuf[opos:opos+lit] = data[ipos:ipos+lit]
+                ipos += lit
+                opos += lit
+            length = ((control >> 2) & 7) + 3
+            offset = b1 + ((control & 0x60) << 3) + 1
+            src = opos - offset
+            for _ in range(length):
+                outbuf[opos] = outbuf[src]
+                opos += 1
+                src += 1
+        
+        elif not (control & MEDIUM_COPY):
+            b2, b3 = data[ipos:ipos+2]
+            ipos += 2
+            lit = b2 >> 6
+            if lit:
+                outbuf[opos:opos+lit] = data[ipos:ipos+lit]
+                ipos += lit
+                opos += lit
+            length = (control & 0x3F) + 4
+            offset = ((b2 & 0x3F) << 8 | b3) + 1
+            src = opos - offset
+            for _ in range(length):
+                outbuf[opos] = outbuf[src]
+                opos += 1
+                src += 1
+        
+        elif not (control & LONG_COPY):
+            b2, b3, b4 = data[ipos:ipos+3]
+            ipos += 3
+            lit = control & 3
+            if lit:
+                outbuf[opos:opos+lit] = data[ipos:ipos+lit]
+                ipos += lit
+                opos += lit
+            length = b4 + ((control & 0x0C) << 6) + 5
+            offset = (((control & 0x10) << 12) | (b2 << 8) | b3) + 1
+            src = opos - offset
+            for _ in range(length):
+                outbuf[opos] = outbuf[src]
+                opos += 1
+                src += 1
+        
+        else:  # literal copy
+            lit = (control & 0x1F) * 4 + 4
+            if lit > 0x70:
+                break
+            outbuf[opos:opos+lit] = data[ipos:ipos+lit]
+            ipos += lit
+            opos += lit
+    
+    # handle trailing bytes
+    trailing = last_control & 3
+    if trailing and opos < out_len:
+        end_pos = min(opos + trailing, out_len)
+        outbuf[opos:end_pos] = data[ipos:ipos + (end_pos - opos)]
+    
+    return bytes(outbuf), size
+
+
+def apply_ps4_header(squads_path):
+    """
+    Sostituisce l'header del file Squads PS4 con quello del file DATA reale.
+    - Rimuove i primi 1178 byte (header generato da save_squads)
+    - Prende l'header di DATA fino al marker T3DB (44 42 00 08)
+    - Azzera i 4 byte che seguono "Type_Squads\x00" nell'header
+    - Rinomina DATA in OLD_DATA e scrive il nuovo file DATA
+    """
+    if not os.path.isfile(DATA_PATH):
+        print("DATA non trovato, skip header replacement")
+        return
+
+    print("Applico header PS4 da DATA...")
+
+    with open(squads_path, "rb") as f:
+        squads = f.read()
+
+    # L'header generato da save_squads ha dimensione fissa: 1126 + 48 + 4 = 1178
+    squads_header_size = 1126 + 48 + 4
+    db_content = squads[squads_header_size:]
+
+    if db_content[:2] != b"DB":
+        print("  ERRORE: DB non trovato al byte {} di Squads".format(squads_header_size))
+        return
+    print("  DB confermato a offset {} (0x{:X}) in Squads".format(squads_header_size, squads_header_size))
+
+    with open(DATA_PATH, "rb") as f:
+        data = f.read()
+
+    # Cerca il marker T3DB (44 42 00 08) nel DATA, ignorando eventuali "DB" nel nome salvataggio
+    t3db_pos = data.find(b"\x44\x42\x00\x08")
+    if t3db_pos < 1000:
+        print("  ERRORE: T3DB non trovato dopo byte 1000 (trovato a {})".format(t3db_pos))
+        return
+    print("  T3DB trovato in DATA a byte {} (0x{:X})".format(t3db_pos, t3db_pos))
+
+    # Copia l'header di DATA e azzera i 4 byte dopo "Type_Squads\x00"
+    new_header = bytearray(data[:t3db_pos])
+    type_squads_sig = b"Type_Squads\x00"
+    ts_pos = new_header.find(type_squads_sig)
+    if ts_pos == -1:
+        print("  ERRORE: 'Type_Squads\\x00' non trovato nell'header di DATA")
+        return
+    crc_pos = ts_pos + len(type_squads_sig)
+    print("  Type_Squads\\x00 a byte {}, CRC={} -> azzerato".format(ts_pos, new_header[crc_pos:crc_pos+4].hex()))
+    new_header[crc_pos:crc_pos+4] = b"\x00" * 4
+    new_data = bytes(new_header) + db_content
+
+    if new_data[t3db_pos:t3db_pos+2] != b"DB":
+        print("  ERRORE: DB non al posto atteso nel nuovo DATA")
+        return
+    print("  DB confermato a byte {} nel nuovo DATA".format(t3db_pos))
+
+    old_data_path = os.path.join(os.path.dirname(os.path.abspath(DATA_PATH)), "OLD_DATA")
+    os.rename(DATA_PATH, old_data_path)
+
+    with open(DATA_PATH, "wb") as f:
+        f.write(new_data)
+
+    old_size = len(data)
+    new_size = len(new_data)
+    if old_size == new_size:
+        print("  Size check PASSATO: {} byte".format(new_size))
+    else:
+        print("  Size check ATTENZIONE: OLD_DATA={}, nuovo DATA={}".format(old_size, new_size))
+
+
+if __name__ == "__main__":
+    if not os.path.isdir(RESULT_DIR):
+        os.mkdir(RESULT_DIR)
+    
+    result = process_rosterupdate()
+    
+    for platform in result["platforms"]:
+        # Scarica solo PS4 e PS5
+        if platform["name"] not in ALLOWED_PLATFORMS:
+            continue
+        
+        platform_path = os.path.join(RESULT_DIR, platform["name"])
+        if not os.path.isdir(platform_path):
+            os.mkdir(platform_path)
+        
+        tags = platform["tags"]
+        
+        # Process Squads
+        ver = tags["dbMajor"]
+        ver_path = os.path.join(platform_path, "squads", ver)
+        if not os.path.isdir(ver_path):
+            os.makedirs(ver_path)
+            loc = tags["dbMajorLoc"]
+            bin_fname = os.path.basename(loc)
+            bin_path = os.path.join(ver_path, bin_fname)
+            download(bin_path, "{}{}".format(CONTENT_URL, loc))
+            fdate = bin_fname.split("_")[1]
+            
+            buf, sz = unpack(bin_path)
+            squads_filename = "Squads{}000000".format(fdate)
+            save_squads(buf, ver_path, squads_filename)
+
+            if platform["name"] == "ps4":
+                apply_ps4_header(os.path.join(ver_path, squads_filename))
+
+        # Process FUT
+        ver = tags["dbFUTVer"]
+        ver_path = os.path.join(platform_path, "FUT", ver)
+        if not os.path.isdir(ver_path):
+            os.makedirs(ver_path)
+            loc = tags["dbFUTLoc"]
+            bin_fname = os.path.basename(loc)
+            bin_path = os.path.join(ver_path, bin_fname)
+            download(bin_path, "{}{}".format(CONTENT_URL, loc))
+            fdate = bin_fname.split("_")[1]
+            
+            buf, sz = unpack(bin_path)
+            save_squads(buf, ver_path, "FutSquads{}000000".format(fdate))
